f5ce85ae82864e1c6026756734e029e0689c4604
Joachim Buyse <jobuyse@gmail.com>
2025-09-13 23:23:30 +0200

    refactor: remove unused model

--- fiftyfive/models.py
+++ fiftyfive/models.py
@@ -4,13 +4,3 @@ from typing import Literal
 
 Mode = Literal["customer", "rechargeSpot"]
 TimeGrouping = Literal["day", "week", "month", "year"]
-
-Notification_nl = Literal[
-    "Gereed",
-    "Transactie voorbereiden",
-    "Remote transactie starten geaccepteerd",
-    "Laden",
-    "Laden (vol)",
-    "Transactie afronden",
-    "Commit transactie",
-]
